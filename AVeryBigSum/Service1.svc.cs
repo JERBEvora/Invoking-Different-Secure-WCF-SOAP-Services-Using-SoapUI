361d929af4531daf08ce426db1d644e6efcd8032
Jailson Evora <jevora@ua.pt>
2020-03-26 13:36:25 -0100

    Initial Commit

---
+++ AVeryBigSum/Service1.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AVeryBigSum
+{
+    public class Service1 : IService1
+    {
+        public long AVeryBigSum(long[] ar)
+        {
+            long aVeryBigSum = 0;
+
+            foreach (long i in ar) aVeryBigSum += i;
+            
+            return aVeryBigSum;
+        }
+    }
+}
---
+++ AVeryBigSum/ServiceAuthenticator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Selectors;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace AVeryBigSum
+{
+    public class ServiceAuthenticator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+
+                throw new SecurityTokenException("Username and password required");
+
+            if (!(userName == ConfigurationManager.AppSettings["AVeryBigNum_User"] && password == ConfigurationManager.AppSettings["AVeryBigNum_Pass"]))
+
+                throw new FaultException(string.Format("Wrong username ({0}) or password ", userName));
+
+        }
+    }
+}
